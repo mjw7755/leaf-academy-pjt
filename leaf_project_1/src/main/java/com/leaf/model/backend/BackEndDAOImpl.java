e7cc7927830ad21ed33bd174baa18b214582f318
user <user@192.168.0.134>
2017-11-21 12:34:04 +0900

    fix push

--- leaf_project_1/src/main/java/com/leaf/model/backend/BackEndDAOImpl.java
+++ leaf_project_1/src/main/java/com/leaf/model/backend/BackEndDAOImpl.java
@@ -5,11 +5,12 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.leaf.model.curriculum.CurriDTO;
 import com.leaf.model.curriculum.LectDTO;
 import com.leaf.model.member.MemberDTO;
-
+@Component
 public class BackEndDAOImpl implements BackEndDAO {
 	@Autowired
 	private SqlSession sqlsession;
