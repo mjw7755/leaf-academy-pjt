5b92856a13f1dbce4ed0603dcafe09b22215fd96
user <user@192.168.0.134>
2017-11-21 18:46:49 +0900

    studentDTO add

---
+++ leaf_project_1/src/main/java/com/leaf/model/stumgmt/StudentDTO.java
@@ -0,0 +1,44 @@
+package com.leaf.model.stumgmt;
+
+public class StudentDTO {
+	private String member_id, student_excel;
+	private int student_id, lect_id;
+	
+	public StudentDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getStudent_excel() {
+		return student_excel;
+	}
+
+	public void setStudent_excel(String student_excel) {
+		this.student_excel = student_excel;
+	}
+
+	public int getStudent_id() {
+		return student_id;
+	}
+
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+
+	public int getLect_id() {
+		return lect_id;
+	}
+
+	public void setLect_id(int lect_id) {
+		this.lect_id = lect_id;
+	}
+	
+	
+}
