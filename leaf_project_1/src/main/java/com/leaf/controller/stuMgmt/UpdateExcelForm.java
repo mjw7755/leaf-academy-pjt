d90582c894724e61741364a45a1700ca52f7f5ab
user <user@192.168.0.125>
2017-11-25 15:22:39 +0900

    updateExcelForm.java update

--- leaf_project_1/src/main/java/com/leaf/controller/stuMgmt/UpdateExcelForm.java
+++ leaf_project_1/src/main/java/com/leaf/controller/stuMgmt/UpdateExcelForm.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.hssf.usermodel.HSSFRichTextString;
@@ -25,20 +26,20 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class UpdateExcelForm{
 
 	
-	public UpdateExcelForm(Map<String,Object> map, HttpServletRequest request) throws EncryptedDocumentException, InvalidFormatException, FileNotFoundException, IOException {
-		updateExcel(map,request);
+	public UpdateExcelForm(Map<String,Object> map, HttpServletRequest request, HttpServletResponse response) throws EncryptedDocumentException, InvalidFormatException, FileNotFoundException, IOException {
+		updateExcel(map,request,response);
 	}
 	
 	
 	
-	public void updateExcel(Map<String,Object> map,HttpServletRequest request) throws EncryptedDocumentException, InvalidFormatException, FileNotFoundException, IOException {
+	public void updateExcel(Map<String,Object> map,HttpServletRequest request, HttpServletResponse response) throws EncryptedDocumentException, InvalidFormatException, FileNotFoundException, IOException {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Date day = new Date();
 		String cur_date = sdf.format(day);
 		String excelName = (String) map.get("excelName");
 		List<String> datelist = (List<String>) map.get("datelist");
 		String student_name = (String)map.get("student_name");
-		String chk_str = (String) map.get("chk_str");
+		List<String> chk_arr = (List<String>) map.get("chk_arr");
 		File exfile = new File("D://software etc/"+excelName+".xls");
 		
 		Workbook workbook = WorkbookFactory.create(new FileInputStream(exfile));
@@ -51,62 +52,23 @@ public class UpdateExcelForm{
 		
 		CellStyle cellStyle = workbook.createCellStyle();
 		Row updateRow = updateSheet.createRow(startRow);
-		
 		Cell updateCell = updateRow.createCell(startcolumn); 
-		for(String date : datelist) {
-			updateCell = updateRow.createCell(startcolumn); 
-	    	if(date.equals(cur_date)) {
-	    		HSSFRichTextString text = new HSSFRichTextString(chk_str);	    		
+		
+		for(String chk : chk_arr) {
+	    		updateCell = updateRow.createCell(startcolumn); 
+	    		HSSFRichTextString text = new HSSFRichTextString(chk);	    		
 	    		updateCell.setCellStyle(cellStyle);
 	    		updateCell.setCellValue(text);            
-	    	}
+	    	
 	    	startcolumn++;
 	    }
 		
 		FileOutputStream fileOut = new FileOutputStream(exfile);
 		workbook.write(fileOut);
 
+		
+		response.sendRedirect("stuMgmtForm.do");
 		
 	}
-	/*@Override
-	protected void buildExcelDocument(Map<String, Object> map, HSSFWorkbook workbook, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date day = new Date();
-		String cur_date = sdf.format(day);
-		String excelName = (String) map.get("excelName");
-		List<String> datelist = (List<String>) map.get("datelist");
-		String student_name = (String)map.get("student_name");
-		String chk_str = (String) map.get("chk_str");
-		File exfile = new File("D://software etc/"+excelName+".xls");
-		
-		workbook = new HSSFWorkbook(new FileInputStream(exfile));
-		
-		int startRow = 3;
-		int startcolumn = 0;
-		HSSFSheet updateSheet = workbook.getSheetAt(0);
-		
-		HSSFCellStyle cellStyle = workbook.createCellStyle();
-		HSSFRow updateRow = updateSheet.createRow(startRow);
-		
-		HSSFCell updateCell = updateRow.createCell(startcolumn); 
-		HSSFRichTextString nameText = new HSSFRichTextString(student_name+"("+excelName+")");
-		updateCell.setCellValue(nameText);  
-		startcolumn++;
-		for(String date : datelist) {
-			updateCell = updateRow.createCell(startcolumn); 
-	    	if(date.equals(cur_date)) {
-	    		System.out.println(chk_str);
-	    		HSSFRichTextString text = new HSSFRichTextString(chk_str);	    		
-	    		updateCell.setCellStyle(cellStyle);
-	    		updateCell.setCellValue(text);            
-	    	}
-	    	startcolumn++;
-	    }
-		
-		FileOutputStream fileOut = new FileOutputStream(exfile);
-		workbook.write(fileOut);
-
-	}*/
 
 }
